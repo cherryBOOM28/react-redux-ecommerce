4e3d68446a85a72e5aa18c36e409df9530c55e52
Louisa Sapina <luiza.sapina.ls@gmail.com>
2025-12-28 23:13:25 +0500

    add: added product card

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import ProductGrid from './components/ProductGrid'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <ProductGrid />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import { BiCart } from 'react-icons/bi'
+
+const Header = () => {
+  return (
+    <div className="bg-white/80 backdrop-blur-md shadow-lg border-b border-gray-200/50 sticky top-0 z-40 py-2">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16 ">
+          <div className="flex items-center gap-1">
+            <h2 className="text-4xl font-bold text-gray-900">eCart</h2>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Search product"
+              className="bg-gray-200 p-3 rounded-md focus:outline-none focus:ring-2 w-2xl"
+            />
+          </div>
+          <button className="relative p-2 bg-gray-200 text-gray-700 rounded-full hover:shadow-lg transform hover:scale-105 transition-all duration-200 cursor-pointer">
+            <BiCart className="w-6 h-6" />
+            <span className="absolute -top-2 -right-2 bg-violet-500 text-white text-xs font-semibold rounded-full w-6 h-6 flex items-center justify-center">
+              1
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
